b9d964bf1599585af577e7b83141ccedd2eb7aa2
Heet Patel <Heet Patel@WINDOWS-43PV03O>
2024-05-14 14:41:45 -0400

    do it after

--- Catch/Form1.cs
+++ Catch/Form1.cs
@@ -16,12 +16,14 @@ namespace Catch
         Rectangle hero = new Rectangle(280, 540, 40, 10);
         int heroSpeed = 10;
 
-        //Ball variables
+        //Ball variables 
         int ballSize = 10;
         int ballSpeed = 8;
 
         //List of balls
         List<Rectangle> ballList = new List<Rectangle>();
+        List<int> ballSpeeds = new List<int>();
+        List<string> ballColours = new List<string>();
 
         int score = 0;
         int time = 500;
@@ -31,6 +33,8 @@ namespace Catch
 
         SolidBrush greenBrush = new SolidBrush(Color.Green);
         SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush goldBrush = new SolidBrush(Color.Gold);
 
         Random randGen = new Random();
         int randValue = 0;
@@ -40,11 +44,6 @@ namespace Catch
         public Form1()
         {
             InitializeComponent();
-            Rectangle ball = new Rectangle(50, 0, ballSize, ballSize);
-            ballList.Add(ball);
-
-            ball = new Rectangle(150, 0, ballSize, ballSize);
-            ballList.Add(ball);
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -73,7 +72,6 @@ namespace Catch
             }
         }
 
-
         private void gameTime_Tick(object sender, EventArgs e)
         {
             //drop the ball objects down the screen
@@ -85,6 +83,89 @@ namespace Catch
                 //update the ball object
                 ballList[i] = new Rectangle(ballList[i].X, y, ballSize, ballSize);
             }
+            //Move Player
+            if (leftPressed == true && hero.X > 0)
+            {
+                hero.X -= heroSpeed;
+            }
+            if (rightPressed == true && hero.X < this.Width - hero.Width)
+            {
+                hero.X += heroSpeed;
+            }
+
+            //create new ball if it is time and different colour 
+            randValue = randGen.Next(0, 100);
+
+            if (randValue < 20)
+            {
+                if (randValue < 10)
+                {
+                    randValue = randGen.Next(0, this.Width);
+
+                    Rectangle ball = new Rectangle(randValue, 0, ballSize, ballSize);
+                    ballList.Add(ball);
+                    ballColours.Add("green");
+                    ballSpeeds.Add(randGen.Next(5, 15));
+                }
+                else if (randValue < 13)
+                {
+                    randValue = randGen.Next(0, this.Width);
+
+                    Rectangle ball = new Rectangle(randValue, 0, ballSize, ballSize);
+                    ballList.Add(ball);
+                    ballColours.Add("red");
+                    ballSpeeds.Add(randGen.Next(5, 15));
+                }
+                else if (randValue < 15)
+                {
+                    randValue = randGen.Next(0, this.Width);
+
+                    Rectangle ball = new Rectangle(randValue, 0, ballSize, ballSize);
+                    ballList.Add(ball);
+                    ballColours.Add("gold");
+                    ballSpeeds.Add(randGen.Next(5, 15));
+                }
+            }
+
+            //remove ball from list if it has gone off the screen
+            for (int i = 0; i < ballList.Count(); i++)
+            {
+               if (ballList[i].Y > this.Height - groundHeight)
+                {
+                    ballList.RemoveAt(i);
+                }
+            } 
+            
+            // check for collision between ball and player
+            for (int i = 0; i < ballList.Count(); i++)
+            {
+                if (ballList[i].IntersectsWith(hero))
+                {
+                    if (ballColours[i] == "green")
+                    {
+                        score++;
+                    }
+                    else if (ballColours[i] == "red")
+                    {
+                        score--;
+                    }
+                    else
+                    {
+                        time = time + 20;
+                    }
+                    ballList.RemoveAt(i);
+                    ballColours.RemoveAt(i);
+                    ballSpeeds.RemoveAt(i);
+                }
+            }
+
+            //decrease time and check if time has run out
+            time--;
+            if (time == 0)
+            {
+                gameTimer.Stop();
+            }
+
 
             //redraw the screen
             Refresh();
@@ -106,8 +187,24 @@ namespace Catch
             //draw balls
             for (int i = 0; i < ballList.Count(); i++)
             {
-                e.Graphics.FillEllipse(greenBrush, ballList[i]);
+                if (ballColours[i] == "green")
+                {
+                    e.Graphics.FillEllipse(greenBrush, ballList[i]);
+                }
+                else if (ballColours[i] == "red")
+                {
+                    e.Graphics.FillEllipse(redBrush, ballList[i]);
+                }
+                else if (ballColours[i] == "gold")
+                {
+                    e.Graphics.FillEllipse(goldBrush, ballList[i]);
+                }
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
